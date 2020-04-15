826a4a887b42d22dce9fceae4fccec9f8537f2c3
Tim <tim.cartr@gmail.com>
2020-04-15 13:46:12 -0600

    app created

---
+++ build/precache-manifest.ec03f6b8d1c6a654ba478babfd6b57de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7463bea8a97674ab737c760fd265e24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "886bc3f82b4d7e70c640",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5920c032d4157ae032b7",
+    "url": "/static/js/2.a8628e5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a8628e5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "886bc3f82b4d7e70c640",
+    "url": "/static/js/main.021dbb0d.chunk.js"
+  },
+  {
+    "revision": "817d2bf81990a1d39ee2",
+    "url": "/static/js/runtime-main.6537f79a.js"
+  }
+]);
